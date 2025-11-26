585b1304447d9e192c82ff3dda214692ac6606eb
thai <thai@example.com>
2025-11-26 19:47:08 +0700

    Trang thông tin cá nhân

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -180,3 +180,20 @@ module.exports.resetPassword = async(req, res) => {
         code:200
     })
 }
+
+//[GET] /api/v1/users/detail
+module.exports.detail = async(req, res) => {
+
+    const token = req.cookies.token;
+
+    const user = await User.findOne({
+        token: token,
+        deleted: false   
+    }).select("-password -token")
+    
+    res.json({
+        code: 200,
+        info: user
+    })
+}
+
